1293a08c4e7a6808ad5db2b3575dd9e14ffcdb30
Homam Mousa <homammousa15@gmail.com>
2025-08-11 15:33:25 +0200

    added state to cart for instantaneous cart deletion

--- resources/js/pages/Cart.tsx
+++ resources/js/pages/Cart.tsx
@@ -1,8 +1,10 @@
+import { useState } from 'react';
+
 type CartItem = { weaponName: string; selectedAttachments: Record<string, number> };
 
 export default function Cart() {
     const cartItem = localStorage.getItem('cart');
-    const cartArray: CartItem[] = cartItem ? JSON.parse(cartItem) : [];
+    const [cartArray, setCartArray] = useState<CartItem[]>(cartItem ? JSON.parse(cartItem) : []);
 
     return (
         <>
@@ -19,10 +21,18 @@ export default function Cart() {
                             <br />
                         </div>
                     ))}
-                    <button onClick={() => localStorage.removeItem('cart')}> Delete all elements from cart</button>
+                    <button
+                        onClick={() => {
+                            localStorage.removeItem('cart');
+                            setCartArray([]);
+                        }}
+                    >
+                        {' '}
+                        Delete all elements from cart
+                    </button>
                 </>
             ) : (
-                'Cart is empty'
+                <div className="flex h-full min-h-screen items-center justify-center">Cart is empty</div>
             )}
             <br />
         </>
