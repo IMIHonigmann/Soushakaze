6dea720691f1e1340013fb93ffe45bc72981e8e9
Homam Mousa <129157644+IMIHonigmann@users.noreply.github.com>
2025-12-16 21:59:07 +0900

    added checks to prevent session hijacking

--- app/Http/Controllers/OrdersController.php
+++ app/Http/Controllers/OrdersController.php
@@ -6,13 +6,25 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Inertia\Inertia;
 use Illuminate\Support\Str;
+use Stripe\StripeClient;
 
 class OrdersController extends Controller
 {
     public function placeOrder(Request $request)
     {
-        $userId = $request->user()->id;
         $sessionId = $request->query('session_id');
+        if (!$sessionId) {
+            return redirect()->route('cart')->with('error', 'Invalid checkout session.');
+        }
+
+        $stripe = new StripeClient(env('STRIPE_SECRET'));
+        $session = $stripe->checkout->sessions->retrieve($sessionId);
+
+        if ($session->payment_status !== 'paid') {
+            return redirect()->route('cart')->with('error', 'Payment not completed.');
+        }
+
+        $userId = $request->user()->id;
         $pendingCart = DB::table('pending_carts')->where('user_id', $userId)->first();
         $customizedWeapons = json_decode($pendingCart->trimmed_cart_data);
         $ordersToInsert = [];
